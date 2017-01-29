38685fe3ff8a4a05aca547df90353b05c6b3fe7c
Matthew Powers <mtpowers@gmail.com>
2017-01-29 12:20:15 -0500

    adding in changes for chapter 5 - django admin

--- learningpython/books/views.py
+++ learningpython/books/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+
+def search_form(request):
+    return render(request, 'search_form.html')
